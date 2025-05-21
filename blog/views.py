a5bfb42bce3414eb13fc654dfa36915ff0261850
Mohammad Ghasemi <sirvanzeroone@outlook.com>
2025-05-21 21:18:21 +0330

    Update views and urls.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
+from blog.models import Post
+
+
 # Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(status='published')
+    context = {
+        'posts': posts,
+    }
+    return HttpResponse("HElloooo")
+
+def post_detail(request, year, month, day , slug):
+    try:
+        post = Post.objects.get(created_date__year=year, created_date__month=month, created_date__day=day,slug=slug,status='published')
+    except Post.DoesNotExist:
+        raise Http404("Post does not exist")
+
+    context = {
+             'post': post,
+    }
+    return HttpResponse('<h1> {} </h1>'.format(post.title))
